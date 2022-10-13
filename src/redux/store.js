8ca40947c73f42072254a1722224b34b74561f29
Gökalp Avcu <gokalp.avcu@santsg.com>
2022-10-13 10:46:11 +0300

    add changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit"; 
 
-import {characterSlice} from "./characterSlice";
+import {characterSlice} from "./charactersSlice"; 
 
 export const store = configureStore({  
     reducer: {

4b1a16fdbf96c2a7bd784f661c1a9020b47305a7
Gökalp Avcu <gokalp.avcu@santsg.com>
2022-10-13 18:27:55 +0300

    add env

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
-import React from "react";
+import { useEffect } from "react";
 
-import {useSelector} from 'react-redux'; // store üzerindeki bir veriye ulaşmak için useSelector kullanılır.
+import { useSelector, useDispatch } from "react-redux"; // store üzerindeki bir veriye ulaşmak için useSelector kullanılır.
+import { fetchCharacters } from "../redux/charactersSlice";
 
 function Home() {
-  const data = useSelector((state) => state.characters); // store üzerindeki characters verisine ulaşmak için state.characters yazılır. 
-  console.log(data);
-  
+  const data = useSelector((state) => state.characters); // store üzerindeki characters verisine ulaşmak için state.characters yazılır.
+  const dispatch = useDispatch(); // store üzerindeki bir veriyi değiştirmek için useDispatch kullanılır.
+
+  useEffect(() => {
+    dispatch(fetchCharacters()); // store üzerindeki bir veriyi değiştirmek için useDispatch kullanılır.
+    console.log(data);
+  }, [dispatch]); // dispatch değiştiğinde useEffect çalışır.
+
   return (
     <div>
       <h1>Home</h1>
--- src/redux/charactersSlice.js
+++ src/redux/charactersSlice.js
@@ -1,14 +1,29 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const fetchCharacters = createAsyncThunk("characters/getCharacters")
+export const fetchCharacters = createAsyncThunk(
+  "characters/getCharacters",
+  async () => {
+    const response = await axios(
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10`
+    );
+    return response.data;
+  }
+);
 
-
-export const charactersSlice = createSlice({  // export ediyorum ki, diğer dosyalarda da kullanabileyim.
-    name: "character",
-    initialState: {
-        items: [],
+export const charactersSlice = createSlice({
+  // export ediyorum ki, diğer dosyalarda da kullanabileyim.
+  name: "characters",
+  initialState: {
+    items: [],
+  },
+  reducers: {},
+  extraReducers: {
+    [fetchCharacters.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      console.log(action.payload);
     },
-    reducers: {},
+  },
 });
 
 export default charactersSlice.reducer; // export ediyorum ki, diğer dosyalarda da kullanabileyim.

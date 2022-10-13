fbad422fbd176278b1908caa73c568a8b065c77b
Gökalp Avcu <gokalp.avcu@santsg.com>
2022-10-13 11:00:02 +0300

    add changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,10 @@
+import React from "react";
+
+import {useSelector} from 'react-redux'; // store üzeridneki bir veriye ulaşmak için useSelector kullanılır.
+
 function Home() {
+  const data = useSelector(state => state.characters); // store üzerindeki characters verisine ulaşmak için state.characters yazılır. 
+  console.log(data)
   return (
     <div>
       <h1>Home</h1>

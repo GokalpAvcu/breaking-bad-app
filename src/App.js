95296b6524826d80b1485097b3a7454020753a7a
Gökalp Avcu <avcu219@gmail.com>
2022-10-14 18:15:44 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         {/* <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<Home />} /> */}
+        <Route path="*" element={<Home />} /> */} 
       </Routes>
     </>
   );

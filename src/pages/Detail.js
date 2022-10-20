d6b64354989ae831bf72372bbaa651681a3dd10b
Gökalp Avcu <avcu219@gmail.com>
2022-10-20 15:15:10 +0300

    add changes

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,10 @@
-
+import { useEffect, useState } from 'react';
 
 import {useParams} from 'react-router-dom';
 
+
 function Detail() {
+  const [char, setChar] = useState(null);
   const {char_id} = useParams(); // useParams ile url'deki parametreye ulaşabiliyorum.
     return (
       <div>
